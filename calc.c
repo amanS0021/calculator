00ca8dcef290941dfb37509b6f430d2341ee57a5
Aman Shahriyar <aman@aman.local>
2022-08-27 15:39:35 +0100

    test

---
+++ calc.c
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double n1, n2;
+    cout << "enter first number: ";
+    cin >> n1;
+    cout << "enter second number: ";
+    
+    return 0;
+}
